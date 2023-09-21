ad94e4a0e2d138d07088b0b10dcced829e22612e
Ricky Rhonaldo Ortega Vital <ortegavitalricky@gmail.com>
2023-09-20 21:09:52 -0500

    Adminlte-proyecto-backend

---
+++ src/main/java/org/rickyortega/adminlte3/AdminLte3Application.java
@@ -0,0 +1,13 @@
+package org.rickyortega.adminlte3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminLte3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminLte3Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/rickyortega/adminlte3/AdminLte3ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rickyortega.adminlte3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminLte3ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
